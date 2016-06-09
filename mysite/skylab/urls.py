6148d291dc582a8cc36009b3e068b8ceecdc51e9
vincentpaul12 <vincentpaul.1217@gmail.com>
2016-06-09 10:25:52 +0800

    added fileupload todo: RAY form change filedropdown with current value of multifilefield hide dropdown when parameter value is set to -i or -s

--- mysite/skylab/urls.py
+++ mysite/skylab/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 
 from skylab.modules.gamess.views import GamessView
-from skylab.modules.ray.views import RayView, UploadView
+from skylab.modules.ray.views import RayView
 from .views import CreateMPIView, HomeView, ToolActivityDetail, serve_private_file
 
 urlpatterns = [
@@ -13,7 +13,6 @@ urlpatterns = [
     url(r'^toolactivity/(?P<pk>\d+)/$', ToolActivityDetail.as_view(), name='toolactivity_detailview'),
     url(r'^use-gamess$', GamessView.as_view(), name='use-gamess'),
     url(r'^ray/use_ray$', RayView.as_view(), name='use-ray'),
-    url(r'^ray/upload_files', UploadView.as_view(), name='ray_upload'),
     url(r'^$', HomeView.as_view(), name='skylab-home'),
     url(r'^create-mpi-cluster$', CreateMPIView.as_view(), name='create_mpi_cluster'),
     url(r'^auth/', include('registration.backends.hmac.urls')),
