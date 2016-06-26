a389bcb93ed128c49d888b9341472e12f57fc2ba
vincentpaul12 <vincentpaul.1217@gmail.com>
2016-06-26 15:34:15 +0800

    Merged vina basic form and advanced form to vina form

--- mysite/skylab/modules/vina/forms.py
+++ mysite/skylab/modules/vina/forms.py
@@ -8,28 +8,30 @@ from skylab.models import MPI_Cluster
 from django.db.models import Q
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+
 # for value type reference:  https://github.com/ryancoleman/autodock-vina/blob/master/src/main/main.cpp
 # see line 459 onwards
 
-class VinaBasicForm(forms.Form):
+class VinaForm(forms.Form):
 
     # Input (receptor and ligand(s) are required)
-    param_receptor = forms.FileField(validators=[pdbqt_file_extension_validator], label="Receptor file",
+    param_receptor = forms.FileField(required=False, validators=[pdbqt_file_extension_validator], label="Receptor file",
                                      help_text="Rigid part of the receptor (.pdbqt)")
-    param_flex = forms.FileField(validators=[pdbqt_file_extension_validator], required=False, label="Flex file",
+    param_flex = forms.FileField(required=False, validators=[pdbqt_file_extension_validator], label="Flex file",
                                  help_text="Flexible side chains, if any (.pdbqt)")
-    param_ligands = MultiFileField(min_num=1, validators=[multi_pdbqt_file_validator], label="Ligand file(s)",
+    param_ligands = MultiFileField(required=False, min_num=1, validators=[multi_pdbqt_file_validator],
+                                   label="Ligand file(s)",
                                    help_text="(.pdbqt)")
 
 
     # Search space (required)
-    param_center_x = forms.DecimalField(label="Center X coordinate")
-    param_center_y = forms.DecimalField(label="Center Y coordinate")
-    param_center_z = forms.DecimalField(label="Center Z coordinate")
+    param_center_x = forms.DecimalField(required=False, label="Center X coordinate")
+    param_center_y = forms.DecimalField(required=False, label="Center Y coordinate")
+    param_center_z = forms.DecimalField(required=False, label="Center Z coordinate")
 
-    param_size_x = forms.DecimalField(label="Size X (Angstroms)")
-    param_size_y = forms.DecimalField(label="Size Y (Angstroms)")
-    param_size_z = forms.DecimalField(label="Size Z (Angstroms)")
+    param_size_x = forms.DecimalField(required=False, label="Size X (Angstroms)")
+    param_size_y = forms.DecimalField(required=False, label="Size Y (Angstroms)")
+    param_size_z = forms.DecimalField(required=False, label="Size Z (Angstroms)")
 
     # Output (optional)
     param_out = forms.CharField(required=False, label="Output model filename",
@@ -55,99 +57,7 @@ class VinaBasicForm(forms.Form):
                                             widget=forms.NumberInput(
                                                 attrs={
                                                     'placeholder': 'default: 3.0, range: 1.0-3.0'}))  # 1-3 default 3.0 float-value in cpp
-
-    def __init__(self, *args, **kwargs):
-        self.user = kwargs.get('user')
-        super(VinaBasicForm, self).__init__(*args, **kwargs)
-
-        current_user_as_creator = Q(creator=self.user)
-        cluster_is_public = Q(shared_to_public=True)
-        supports_vina = Q(supported_tools="vina")
-        # is_ready = Q(status=1)
-        q = MPI_Cluster.objects.filter(current_user_as_creator | cluster_is_public)
-        q = q.filter(supports_vina).exclude(status=4)  # exclude unusable clusters
-
-        self.fields['mpi_cluster'] = forms.ModelChoiceField(queryset=q,
-                                                            help_text="Getting a blank list? Try <a href='../create_mpi_cluster'>creating an MPI Cluster</a> first.")
-
-        self.helper = FormHelper()
-        self.helper.form_tag = False
-
-        self.helper.layout = Layout(  # crispy_forms layout
-            Div(
-                Field('mpi_cluster', wrapper_class='col-xs-5'),
-                css_class="col-sm-12"
-            ),
-            Fieldset(
-                'Input',
-                Div(
-                    Div('param_receptor', css_class='col-xs-4'),
-                    Div('param_flex', css_class='col-xs-4'),
-                    Div('param_ligands', css_class='col-xs-4'),
-                    css_class='row-fluid col-sm-12'
-                )
-            ),
-            Fieldset(
-                'Search space',
-                Div(
-                    Div('param_center_x', css_class='col-xs-4'),
-                    Div('param_center_y', css_class='col-xs-4'),
-                    Div('param_center_z', css_class='col-xs-4'),
-                    Div('param_size_x', css_class='col-xs-4'),
-                    Div('param_size_y', css_class='col-xs-4'),
-                    Div('param_size_z', css_class='col-xs-4'),
-                    css_class='row-fluid col-sm-12'
-                )
-            ),
-            Fieldset(
-                'Output',
-                Div(
-                    Div(
-                        Div(
-                            AppendedText('param_out', '.pdbqt', active=True, placeholder="filename"),
-                            css_class='col-xs-8'
-                        ),
-                        css_class='col-xs-6'
-                    ),
-                    Div(
-                        Div(
-                            AppendedText('param_log', '.log', active=True, placeholder="filename"),
-                            css_class='col-xs-8'
-                        ),
-                        css_class='col-xs-6'
-                    ),
-                    css_class='row-fluid col-sm-12'
-                )
-            ),
-            Fieldset(
-                'Miscellaneous',
-                Div(
-                    Div(
-                        Field('param_seed', wrapper_class="col-xs-8"),
-                        css_class='col-xs-6'
-                    ),
-                    Div(
-                        Field('param_exhaustiveness', wrapper_class="col-xs-8"),
-                        css_class='col-xs-6'
-                    ),
-                    Div(
-                        Field('param_num_modes', wrapper_class="col-xs-8"),
-                        css_class='col-xs-6'
-                    ),
-                    Div(
-                        Field('param_energy_range', wrapper_class="col-xs-8"),
-                        css_class='col-xs-6'
-                    ),
-                    css_class='row-fluid col-sm-12'
-                )
-            ),
-
-
-        )
-
-# add cripsy form helper
-
-class VinaAdvancedForm(forms.Form):
+    # Advanced
     param_score_only = forms.BooleanField(required=False, label="--score_only", help_text="Search space can be omitted")
     param_local_only = forms.BooleanField(required=False, label="--local_only ", help_text="Do local search only    ")
     param_randomize_only = forms.BooleanField(required=False, label="--randomize_only",
@@ -167,14 +77,95 @@ class VinaAdvancedForm(forms.Form):
     param_weight_rot = forms.DecimalField(required=False, label="N_rot weight", widget=forms.NumberInput(
         attrs={'placeholder': 'default: 0.058459999999999998'}))
 
+
     def __init__(self, *args, **kwargs):
-        super(VinaAdvancedForm, self).__init__(*args, **kwargs)
+        self.user = kwargs.get('user')
+        super(VinaForm, self).__init__(*args, **kwargs)
+
+        current_user_as_creator = Q(creator=self.user)
+        cluster_is_public = Q(shared_to_public=True)
+        supports_vina = Q(supported_tools="vina")
+        # is_ready = Q(status=1)
+        q = MPI_Cluster.objects.filter(current_user_as_creator | cluster_is_public)
+        q = q.filter(supports_vina).exclude(status=4)  # exclude unusable clusters
+
+        self.fields['mpi_cluster'] = forms.ModelChoiceField(queryset=q,
+                                                            help_text="Getting a blank list? Try <a href='../create_mpi_cluster'>creating an MPI Cluster</a> first.")
 
         self.helper = FormHelper()
         self.helper.form_tag = False
-        self.helper.disable_csrf = True
 
         self.helper.layout = Layout(  # crispy_forms layout
+            Div(
+                Div(
+                    Field('mpi_cluster', wrapper_class='col-xs-5'),
+                    css_class="col-sm-12"
+                ),
+                Fieldset(
+                    'Input',
+                    Div(
+                        Div('param_receptor', css_class='col-xs-4'),
+                        Div('param_flex', css_class='col-xs-4'),
+                        Div('param_ligands', css_class='col-xs-4'),
+                        css_class='row-fluid col-sm-12'
+                    )
+                ),
+                Fieldset(
+                    'Search space',
+                    Div(
+                        Div('param_center_x', css_class='col-xs-4'),
+                        Div('param_center_y', css_class='col-xs-4'),
+                        Div('param_center_z', css_class='col-xs-4'),
+                        Div('param_size_x', css_class='col-xs-4'),
+                        Div('param_size_y', css_class='col-xs-4'),
+                        Div('param_size_z', css_class='col-xs-4'),
+                        css_class='row-fluid col-sm-12'
+                    )
+                ),
+                Fieldset(
+                    'Output',
+                    Div(
+                        Div(
+                            Div(
+                                AppendedText('param_out', '.pdbqt', active=True, placeholder="filename"),
+                                css_class='col-xs-8'
+                            ),
+                            css_class='col-xs-6'
+                        ),
+                        Div(
+                            Div(
+                                AppendedText('param_log', '.log', active=True, placeholder="filename"),
+                                css_class='col-xs-8'
+                            ),
+                            css_class='col-xs-6'
+                        ),
+                        css_class='row-fluid col-sm-12'
+                    )
+                ),
+                Fieldset(
+                    'Miscellaneous',
+                    Div(
+                        Div(
+                            Field('param_seed', wrapper_class="col-xs-8"),
+                            css_class='col-xs-6'
+                        ),
+                        Div(
+                            Field('param_exhaustiveness', wrapper_class="col-xs-8"),
+                            css_class='col-xs-6'
+                        ),
+                        Div(
+                            Field('param_num_modes', wrapper_class="col-xs-8"),
+                            css_class='col-xs-6'
+                        ),
+                        Div(
+                            Field('param_energy_range', wrapper_class="col-xs-8"),
+                            css_class='col-xs-6'
+                        ),
+                        css_class='row-fluid col-sm-12'
+                    )
+                ),
+                css_id='tab1', css_class='tab-pane fade in active'
+            ),
             Div(
                 Div('param_score_only', css_class='col-xs-4'),
                 Div('param_local_only', css_class='col-xs-4'),
@@ -204,12 +195,10 @@ class VinaAdvancedForm(forms.Form):
                     Field('param_weight_rot', wrapper_class='col-xs-10'),
                     css_class='col-xs-6'
                 ),
-                css_class='row-fluid col-sm-12'
+                css_id='tab2',
+                css_class='tab-pane fade row-fluid col-sm-12'
             ),
-
         )
-
-# add crispy form helper
 
 class VinaSplitForm(forms.Form):
     param_input = forms.FileField(validators=[pdbqt_file_extension_validator])
--- mysite/skylab/modules/vina/views.py
+++ mysite/skylab/modules/vina/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView, FormView
-from skylab.modules.vina.forms import VinaBasicForm, VinaAdvancedForm
+from skylab.modules.vina.forms import VinaForm
+from django import forms
+from django.shortcuts import render, redirect
 
 
 class VinaView(TemplateView):
@@ -7,13 +9,22 @@ class VinaView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(VinaView, self).get_context_data(**kwargs)
-        context['vina_basic_form'] = VinaBasicForm()
-        context['vina_advanced_form'] = VinaAdvancedForm()
+        context['vina_form'] = VinaForm()
         context['user'] = self.request.user
         return context
 
     def post(self, request, *args, **kwargs):
-        pass
+        vina_form = VinaForm(request.POST, request.FILES)
+
+        if vina_form.is_valid():
+
+            pass
+            # return redirect("../toolactivity/%d" % tool_activity.id)
+        else:
+            return render(request, 'modules/vina/use_vina.html', {
+                'vina_form': vina_form,
+
+            })
 
 
 class VinaSplitView(FormView):
