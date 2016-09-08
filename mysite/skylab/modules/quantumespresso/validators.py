9691c93ae80c23a022592e90e9eccc814bf3ee32
vincentpaul12 <vincentpaul.1217@gmail.com>
2016-09-08 22:26:43 +0800

    Quantum Espresso - Simplified in file validator

--- mysite/skylab/modules/quantumespresso/validators.py
+++ mysite/skylab/modules/quantumespresso/validators.py
@@ -3,21 +3,8 @@ import os.path
 
 
 def in_file_extension_validator(file):
-    # using .conf is also not supported because it does not fit with the use-case abstraction
-    # export.txt, qseq.txt are not supported because of lack of documentation on their use case
-    # .gz and bz2 files are not supported since compilation in p2c-tools script does not have HAVE_LIBZ=y, HAVE_LIBBZ2=y
-    # valid_file_extensions = ['.fasta', '.fa', '.fasta.gz', '.fa.gz', '.fasta.bz2', '.fa.bz2', '.fastq', '.fq',
-    #                          '.fastq.gz', '.fq.gz', '.fastq.bz2',
-    #                          '.fq.bz2', '.sff', '.csfasta', '.csfa'
-    #                          ]
-    valid_file_extensions = ['.in']
+    ext = os.path.splitext(file.name)[1]
 
-    valid = False
-    for ext in valid_file_extensions:
-        if file.name.lower().endswith(ext):
-            valid = True
-            break
-    if not valid:
-        invalid_ext = os.path.splitext(file.name)[1]
-        raise forms.ValidationError(u'%s not supported' % invalid_ext, code='invalid_filetype')
-
+    if ext.lower() != '.in':
+        raise forms.ValidationError(u'Only .in file accepted',
+                                    code="quantum_espresso_invalid_input_file")
