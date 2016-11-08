8a55d6a0ac8146ab75a5887fec4ebcfa7f4774bf
vincentpaul12 <vincentpaul.1217@gmail.com>
2016-11-09 03:46:22 +0800

    For UAT - Untested (Ray, dock6, vina_split)

--- mysite/skylab/modules/impi/validators.py
+++ mysite/skylab/modules/impi/validators.py
@@ -10,7 +10,6 @@ def impi_files_validator(files):
 
 def jpeg_file_extension_validator(f):
     ext = os.path.splitext(f.name)[1]
-    print ext
     if ext.lower() not in ['.jpeg', '.jpg']:
         raise forms.ValidationError(u'Only JPEG (.jpeg, .jpg) file accepted',
                                     code="impi_invalid_input_file")
